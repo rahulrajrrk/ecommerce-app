0f1d50ec5b0280cd8532d73585ec267665780b60
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-10 20:46:23 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
+function Navbar() {
+  return (
+    <nav>
+      <Link to="/">Home</Link> | <Link to="/products">Products</Link> |{" "}
+      <Link to="/cart">Cart</Link> | <Link to="/order-tracking">Order Tracking</Link> |{" "}
+      <Link to="/login">Login</Link>
+    </nav>
+  );
+}
+
+export default Navbar;
