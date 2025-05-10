92dabf26923fa833b45f4cca301bfdad360e71da
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-10 20:42:58 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,1 +1,20 @@
+import React from "react";
 
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <label>Email: </label>
+        <input type="email" />
+        <br />
+        <label>Password: </label>
+        <input type="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
