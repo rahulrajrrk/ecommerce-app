3b65540ed3fcacdad726dd0af8ad3fc664b9d7e2
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-10 20:42:31 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to the E-Commerce App</h1>
+      <p>Explore our products and shop your favorites!</p>
+    </div>
+  );
+}
+
+export default Home;
