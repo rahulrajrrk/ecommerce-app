5cce373b5c681317a58dbe1540c9b94bb2917ee2
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-10 20:38:37 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,27 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import ProductCatalog from "./pages/ProductCatalog";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import OrderTracking from "./pages/OrderTracking";
+import Navbar from "./components/Navbar";
 
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/products" element={<ProductCatalog />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/order-tracking" element={<OrderTracking />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
