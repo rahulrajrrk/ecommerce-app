fe796a4c04121c6303bca253c14ec27094f17055
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-10 20:43:42 +0530

    Update ProductCatalog.js

--- frontend/src/pages/ProductCatalog.js
+++ frontend/src/pages/ProductCatalog.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function ProductCatalog() {
+  return (
+    <div>
+      <h1>Product Catalog</h1>
+      <p>Here you can browse all products.</p>
+    </div>
+  );
+}
+
+export default ProductCatalog;
