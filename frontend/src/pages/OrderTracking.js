ca44ea1682ebe07902061e5329f3ffb9a08d3877
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-10 20:45:47 +0530

    Update OrderTracking.js

--- frontend/src/pages/OrderTracking.js
+++ frontend/src/pages/OrderTracking.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function OrderTracking() {
+  return (
+    <div>
+      <h1>Order Tracking</h1>
+      <p>Track your orders here.</p>
+    </div>
+  );
+}
+
+export default OrderTracking;
