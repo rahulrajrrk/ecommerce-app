8593ac950708d08c44085a4710be8fefe43041ea
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-10 20:44:42 +0530

    Update Cart.js

--- frontend/src/pages/Cart.js
+++ frontend/src/pages/Cart.js
@@ -1,1 +1,12 @@
+import React from "react";
 
+function Cart() {
+  return (
+    <div>
+      <h1>Your Cart</h1>
+      <p>View and manage items in your cart.</p>
+    </div>
+  );
+}
+
+export default Cart;
