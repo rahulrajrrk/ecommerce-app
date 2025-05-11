a1e002d0497eb6d18ad0412c441938b9ef6987a2
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-11 11:20:02 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,12 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Navbar.css"; // Import the CSS file for styling
 
 function Navbar() {
   return (
-    <nav>
-      <Link to="/">Home</Link> | <Link to="/products">Products</Link> |{" "}
-      <Link to="/cart">Cart</Link> | <Link to="/order-tracking">Order Tracking</Link> |{" "}
-      <Link to="/login">Login</Link>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">E-Commerce</div>
+        <ul className="navbar-links">
+          <li>
+            <Link to="/" className="navbar-link">Home</Link>
+          </li>
+          <li>
+            <Link to="/products" className="navbar-link">Products</Link>
+          </li>
+          <li>
+            <Link to="/cart" className="navbar-link">Cart</Link>
+          </li>
+          <li>
+            <Link to="/order-tracking" className="navbar-link">Order Tracking</Link>
+          </li>
+          <li>
+            <Link to="/login" className="navbar-link">Login</Link>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
