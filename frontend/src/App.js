2d3c41c24a43ec4a40c3bda0d80cb4bfcfe8a05e
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-12 14:42:54 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,45 +2,29 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 
-// Admin Pages
-import AdminLogin from "./pages/Login/AdminLogin";
-import AdminDashboard from "./pages/Admin/AdminDashboard";
-import ProductManagement from "./pages/Admin/ProductManagement";
-import Reports from "./pages/Admin/Reports";
-import OrderTracking from "./pages/Admin/OrderTracking";
-
-// Customer Pages
+// Import Pages
+import ProductListing from "./pages/Customer/ProductListing";
+import Cart from "./pages/Customer/Cart";
 import CustomerLogin from "./pages/Login/CustomerLogin";
 import Register from "./pages/Login/Register";
 import ForgotPassword from "./pages/Login/ForgotPassword";
-import ProductListing from "./pages/Customer/ProductListing";
-import Cart from "./pages/Customer/Cart";
-import Checkout from "./pages/Customer/Checkout";
-import OrderHistory from "./pages/Customer/OrderHistory";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        {/* Admin Routes */}
-        <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<AdminDashboard />} />
-        <Route path="/admin/products" element={<ProductManagement />} />
-        <Route path="/admin/reports" element={<Reports />} />
-        <Route path="/admin/order-tracking" element={<OrderTracking />} />
+        {/* Default Route */}
+        <Route path="/" element={<ProductListing />} />
 
-        {/* Customer Routes */}
+        {/* Customer Pages */}
+        <Route path="/products" element={<ProductListing />} />
+        <Route path="/cart" element={<Cart />} />
+
+        {/* Login Pages */}
         <Route path="/login" element={<CustomerLogin />} />
         <Route path="/register" element={<Register />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/products" element={<ProductListing />} />
-        <Route path="/cart" element={<Cart />} />
-        <Route path="/checkout" element={<Checkout />} />
-        <Route path="/order-history" element={<OrderHistory />} />
-
-        {/* Default Route */}
-        <Route path="/" element={<CustomerLogin />} />
       </Routes>
     </Router>
   );
