2a290118a07a8d452a70f0ef108c2564df2176fe
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-12 12:26:04 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,50 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Login from "./pages/Login";
-import ProductCatalog from "./pages/ProductCatalog";
-import Cart from "./pages/Cart";
-import Checkout from "./pages/Checkout";
-import OrderTracking from "./pages/OrderTracking";
 import Navbar from "./components/Navbar";
+
+// Admin Pages
+import AdminDashboard from "./pages/Admin/AdminDashboard";
+import OrderTracking from "./pages/Admin/OrderTracking";
+import ProductManagement from "./pages/Admin/ProductManagement";
+import Reports from "./pages/Admin/Reports";
+
+// Customer Pages
+import ProductListing from "./pages/Customer/ProductListing";
+import Cart from "./pages/Customer/Cart";
+import Checkout from "./pages/Customer/Checkout";
+import OrderHistory from "./pages/Customer/OrderHistory";
+
+// Login Pages
+import AdminLogin from "./pages/Login/AdminLogin";
+import CustomerLogin from "./pages/Login/CustomerLogin";
+import Register from "./pages/Login/Register";
+import ForgotPassword from "./pages/Login/ForgotPassword";
 
 function App() {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/products" element={<ProductCatalog />} />
+        {/* Admin Routes */}
+        <Route path="/admin/login" element={<AdminLogin />} />
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />
+        <Route path="/admin/products" element={<ProductManagement />} />
+        <Route path="/admin/reports" element={<Reports />} />
+        <Route path="/admin/order-tracking" element={<OrderTracking />} />
+
+        {/* Customer Routes */}
+        <Route path="/products" element={<ProductListing />} />
         <Route path="/cart" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
-        <Route path="/order-tracking" element={<OrderTracking />} />
+        <Route path="/order-history" element={<OrderHistory />} />
+
+        {/* Login Pages */}
+        <Route path="/login" element={<CustomerLogin />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        {/* Default Route */}
+        <Route path="/" element={<ProductListing />} />
       </Routes>
     </Router>
   );
