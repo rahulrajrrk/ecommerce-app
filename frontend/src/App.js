7c26eef04057c1ea07e80adeae6bee810edee17b
rahulrajrrk <rahulrajrrk@gmail.com>
2025-05-12 12:20:47 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import Login from "./pages/Login";
 import ProductCatalog from "./pages/ProductCatalog";
 import Cart from "./pages/Cart";
@@ -13,7 +12,6 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/products" element={<ProductCatalog />} />
         <Route path="/cart" element={<Cart />} />
